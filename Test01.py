b8770840a78a093f151c96ddfbb686b73960c601
JBD-Code <lio222@naver.com>
2020-07-07 14:36:39 +0900

    Python Start

---
+++ Test01.py
@@ -0,0 +1,30 @@
+#Python
+# Python Data Type
+# 정수형(Integer)
+a = 10
+b = 20
+print(a + b)
+print(a - b)
+print(a / b)
+print(a * b)
+print(a % b)
+
+# 실수형 (Floatin-point)
+a = 1.2
+b = 3.656
+print(a+b)
+#Python에서는 8진수를 만들기 위해서는 숫자가 숫자 0 + 알파벳 o 를 붙인다
+a = 0o177
+print(a)
+#Python에서는 16진수를 만들기 위해서는 숫자가 숫자 0 + 알파벳 x 를 붙인다
+b= 0x8ff
+print(b)
+print(a + b)
+# 숫자형을 활용하기 위한 연산자
+# x의 y 제곱을 나타내는 ** 연산자가 있다
+print(a**b)
+
+#Python에서 변수를 선언하는 방법
+a = 4
+b,c = 3,5
+print(a+ b+ c)
