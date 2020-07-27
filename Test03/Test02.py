3e098e70024bad7aa6deed4c93499cef49f7c644
JBD-Code <lio222@naver.com>
2020-07-27 14:59:00 +0900

    Python Study 2020-07-27 Input

---
+++ Test03/Test02.py
@@ -0,0 +1,18 @@
+#Python
+# Python 사용자 입출력
+# input 의 사용
+a = input()
+print(a)
+
+#input 는 입력되는 모든 값을 문자열로 취급한다
+number =input("숫자를 입력하세요")
+print(number)
+
+#print함수 알아보기
+#큰따옴표로 둘러싸인 문자열은 + 연산과 동일하다
+
+print("Hello" "is" "too long")
+print("Hello" , "World")
+
+for i in range(10):
+    print(i, end='')
